1e11bcd097a3d4230bcfac6e265951dda670449e
Gowthaman.R <82644295+GOWTHAMAN07112001@users.noreply.github.com>
2022-12-13 15:31:17 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/screens/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Splash(),
+    );
+  }
+}
